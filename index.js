135d2863b7d983568c00bd4ceb7f108c879d66a9
Johongirr <johongir_rahimov@bk.ru>
2020-04-10 22:08:15 +0200

    added grid

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
  const container = document.querySelector('.container-grid');
  const clearGrid = document.querySelector('.clear-grid');
  const randomColor = document.querySelector('.random-color');
+ 
 
- console.dir(window)
  function createGrid(gridSize){
-     
-    let size = Math.floor((960/gridSize));
-    console.log(size)
-    console.log(size)
+ let size = Math.floor((container.offsetWidth/gridSize));
+   
       for(let i = 0; i < gridSize; i++){
           let row = document.createElement('div');
-          row.classList.add('flex');
+          row.style = `display: grid; grid-template-columns: repeat(${gridSize}, ${size}px);
+                       grid-auto-rows: ${size}px`
           for(let j = 0; j < gridSize; j++){
               let cell = document.createElement('div');
               cell.classList.add('cell')
-              cell.style.height = `${size}px`;
-              cell.style.width = `${size}px`;
+             
+             
               row.appendChild(cell)
           }
           container.appendChild(row);
@@ -43,6 +42,7 @@ createGrid(prompt('Provide grid size max(64x64)'))
                     'lightgreen','darkbrown',];
      this.style.backgroundColor = `${colors[Math.floor(Math.random() * colors.length)]}`;
  }
+ 
  function selectCells (){
     document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('mouseover', addColor))
  }
